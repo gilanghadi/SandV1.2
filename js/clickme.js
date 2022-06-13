9554a5922930092078320123b7b482f20286420f
Gilang Hadi <gilangsaputro987@gmail.com>
2022-06-13 14:35:53 +0700

    update website on image

---
+++ js/clickme.js
@@ -0,0 +1,25 @@
+const clickme = document.getElementById('clickme');
+const clickme1 = document.getElementById('clickme1');
+const clickme2 = document.getElementById('clickme2');
+const clickme3 = document.getElementById('clickme3');
+clickme.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('jan batu klo dibilangin(cmn ngigetin)');
+    alert('yeh jagn marah gua ngomong gitu,pisss');
+})
+clickme1.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('jangan nakal,klo nakal yang wajar aja');
+    alert('love yu');
+})
+clickme2.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('knp burung bunyinya cuit cuit,karna kalo love yu love yu itu aku ke kamu');
+    alert('asiik massuk pak aguss');
+})
+clickme3.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('knp matahari terbenam?karna dia kalah cantik sama lu');
+    alert('asekk masuk lagi');
+    alert('namanya juga usaha (usaha dagangg!!)');
+})
